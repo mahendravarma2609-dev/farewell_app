c467631a46a721ee8026c871de68cec827f3ad6b
mahendravarma2609-dev <mahendravarma2609@gmail.com>
2026-05-10 09:17:10 +0000

    add welcome screen

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,6 @@
   import 'package:flutter/material.dart';
 
-import 'src/presentation/screens/login_page.dart';
+import 'src/presentation/screens/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: const WelcomePage(),
     );
   }
 }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
